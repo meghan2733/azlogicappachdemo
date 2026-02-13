b85a1a2f4314319f95e2ea8f77d42e5efd038b01
Meghan Kulkarni <meghan.kulkarni@outlook.com>
2026-02-13 00:29:42 -0500

    Comment out output definitions in outputs.tf for clarity

--- outputs.tf
+++ outputs.tf
@@ -1,33 +1,33 @@
-output "resource_group_name" {
-  description = "Name of the created resource group"
-  value       = azurerm_resource_group.ach_demo.name
-}
+# output "resource_group_name" {
+#   description = "Name of the created resource group"
+#   value       = azurerm_resource_group.ach_demo.name
+# }
 
-output "storage_account_name" {
-  description = "Name of the storage account"
-  value       = azurerm_storage_account.ach_storage.name
-}
+# output "storage_account_name" {
+#   description = "Name of the storage account"
+#   value       = azurerm_storage_account.ach_storage.name
+# }
 
-output "storage_account_primary_connection_string" {
-  description = "Primary connection string for the storage account"
-  value       = azurerm_storage_account.ach_storage.primary_connection_string
-  sensitive   = true
-}
+# output "storage_account_primary_connection_string" {
+#   description = "Primary connection string for the storage account"
+#   value       = azurerm_storage_account.ach_storage.primary_connection_string
+#   sensitive   = true
+# }
 
-output "blob_container_input" {
-  description = "Name of the input blob container"
-  value       = azurerm_storage_container.ach_input.name
-}
+# output "blob_container_input" {
+#   description = "Name of the input blob container"
+#   value       = azurerm_storage_container.ach_input.name
+# }
 
-output "blob_container_validated" {
-  description = "Name of the validated blob container"
-  value       = azurerm_storage_container.ach_validated.name
-}
+# output "blob_container_validated" {
+#   description = "Name of the validated blob container"
+#   value       = azurerm_storage_container.ach_validated.name
+# }
 
-output "blob_container_failed" {
-  description = "Name of the failed blob container"
-  value       = azurerm_storage_container.ach_failed.name
-}
+# output "blob_container_failed" {
+#   description = "Name of the failed blob container"
+#   value       = azurerm_storage_container.ach_failed.name
+# }
 
 # output "logic_app_name" {
 #   description = "Name of the Logic App"
